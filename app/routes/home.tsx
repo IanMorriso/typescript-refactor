ec75b043b1473460b05111c197d979358522f90a
Ian <ihmorris@ualberta.ca>
2025-03-17 07:14:04 -0600

    tailwind config

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,5 +1,6 @@
 import type { Route } from "./+types/home";
 import { Welcome } from "../welcome/welcome";
+import { Highlights } from "~/highlights/highlights";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -9,5 +10,10 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
-  return <Welcome />;
+  return (
+    <div>
+      <Welcome />
+      <Highlights />
+  </div>
+);
 }
